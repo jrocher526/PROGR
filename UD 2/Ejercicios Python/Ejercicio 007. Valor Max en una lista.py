199443244b5a92c651ef4447fde14b85381356fc
Jhonal Roca <jrocher526@g.educaand.es>
2025-10-29 21:33:12 +0100

    Add files via upload

---
+++ UD 2/Ejercicios Python/Ejercicio 007. Valor Max en una lista.py
@@ -0,0 +1,12 @@
+# Creamos funcion para recorrer la lista y encontrar el valor maximo
+def maximo(t):
+    # Asumimos que t no tiene nada
+    max_valor = t[0]
+    for num in t:
+        if num > max_valor:
+            max_valor = num
+    return max_valor
+
+# Ejemplo de uso
+tabla = [5, 12, 3, 20, 8]
+print("El valor máximo es:", maximo(tabla))
