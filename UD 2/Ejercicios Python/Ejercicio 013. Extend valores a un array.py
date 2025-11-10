2a9dd9460c9fcc8e46573f6c0d06509c1de54270
Jhonal Roca <jrocher526@g.educaand.es>
2025-11-10 18:07:31 +0100

    Delete UD 2/Ejercicios Python/Ejercicio 013. Extend valores a un array.py

--- UD 2/Ejercicios Python/Ejercicio 013. Extend valores a un array.py
+++
@@ -1,8 +0,0 @@
-curso1 = ["Pablo", "David"]
-curso2 = ["Alvaro", "Fran"]
-
-print("Curso 1", curso1, "\nCurso 2:", curso2)
-
-curso1.extend(curso2)
-
-print("Los nuevos alumnos del curso 1:", curso1)
