63362cb26068726ce770af08782531861819a42e
Jhonal Roca <jrocher526@g.educaand.es>
2025-11-10 18:08:39 +0100

    Delete UD 2/Ejercicios Python/Ejercicio 015. pop(Elimina y guarda) valores a un array.py

--- UD 2/Ejercicios Python/Ejercicio 015. pop(Elimina y guarda) valores a un array.py
+++
@@ -1,7 +0,0 @@
-animales = ["Gato", "Perro", "Conejo", "Loro", "Pez"]
-
-ultimo = animales.pop(4)
-primero = animales.pop(1)
-
-print("Lista Original:", animales, "\nAnimales Eliminados:", ultimo, "y", primero)
-
