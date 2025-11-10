a466e5312e0170cb568d9ccefd54d5703958b0a3
Jhonal Roca <jrocher526@g.educaand.es>
2025-11-10 18:09:28 +0100

    Add files via upload

---
+++ UD 2/Ejercicios Refuerzo/Ejercicio 008. Inventarios combinados.py
@@ -0,0 +1,9 @@
+inventario_A = ["Manzana", "Pera", "Melocoton"]
+
+inventario_B = ["Kiwi", "Uvas", "Cambur"]
+
+inventario_Total = []
+
+inventario_Total.extend(inventario_A + inventario_B)
+
+print(inventario_Total)
---
+++ UD 2/Ejercicios Refuerzo/Ejercicio 009. Seeleccion de temporada.py
@@ -0,0 +1,8 @@
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+verano = []
+
+verano.extend(meses[5:8])
+
+print(verano)
