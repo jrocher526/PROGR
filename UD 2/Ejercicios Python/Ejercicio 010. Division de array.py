a052f435cf266a9b4b400ce9d432e445053a5528
Jhonal Roca <jrocher526@g.educaand.es>
2025-11-10 18:06:18 +0100

    Delete UD 2/Ejercicios Python/Ejercicio 010. Division de array.py

--- UD 2/Ejercicios Python/Ejercicio 010. Division de array.py
+++
@@ -1,8 +0,0 @@
-nombreApellido = ["J", "h", "o", "n", "a", "l", " ", "R", "o", "c", "a"]
-
-nombre = nombreApellido[:6]
-apellido = nombreApellido[7:]
-
-print("Nombre completo", nombreApellido)
-print("Nombre", nombre)
-print("Apellido", apellido)
