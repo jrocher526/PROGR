215106fefb4a80cfd309869725833f45eeb8f80c
Jhonal Roca <jrocher526@g.educaand.es>
2025-11-11 14:41:21 +0100

    Add files via upload

---
+++ UD 2/Ejercicios Python/Ejercicio 018. Busqueda mixtas (Lineal y Binaria).py
@@ -0,0 +1,15 @@
+import ejercicio015BusquedaLineal
+import ejercicio016BusquedaBinaria
+
+numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+print("Busqueda lineal:")
+lineal = ejercicio015BusquedaLineal.buscar_numero(premio, numeros)
+print(lineal)
+
+print("\nBusqueda Binaria:")
+binaria = ejercicio016BusquedaBinaria.busquedaBinaria(premio, eleccion)
+print(binaria)
+
+
+
