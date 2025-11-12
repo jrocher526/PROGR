f0fe09b23f940d02a01a3f09f7babeff35cd1060
Jhonal Roca <jrocher526@g.educaand.es>
2025-11-12 13:03:53 +0100

    Add files via upload

---
+++ UD 2/Ejercicios Refuerzo/Ejercicio 010. Comparacion de lista (mayus-minus).py
@@ -0,0 +1,20 @@
+a = ["H", "O", "L", "A"]
+b = ["h", "o", "l", "a"]
+c = ["H", "o", "L", "A"]
+
+def comparar(lista1, lista2):
+    for i in range(len(lista1)):
+        if lista1[i] != lista2[i]:
+            
+    return False
+
+return True
+
+print("Comprobar A y B:", comparar(a, b))
+print("Comprobar A y C:", comparar(a, c))
+print("Comprobar B y C:", comparar(b, c))
+
+
+
+
+
