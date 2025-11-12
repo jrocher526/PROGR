2eb512f5760bfe74c75b15bc57877b417f37a15a
Jhonal Roca <jrocher526@g.educaand.es>
2025-11-12 16:32:18 +0100

    Update Ejercicio 010. Comparacion de lista (mayus-minus).py

--- UD 2/Ejercicios Refuerzo/Ejercicio 010. Comparacion de lista (mayus-minus).py
+++ UD 2/Ejercicios Refuerzo/Ejercicio 010. Comparacion de lista (mayus-minus).py
@@ -1,18 +1,35 @@
 a = ["H", "O", "L", "A"]
-b = ["h", "o", "l", "a"]
+b = ["h", "o", "j", "a"]
 c = ["H", "o", "L", "A"]
+d = ["C", "h", "a"]
 
-def comparar(lista1, lista2):
-    for i in range(len(lista1)):
-        if lista1[i] != lista2[i]:
-            
-    return False
+def compararTamano(lista1, lista2):
+    # Comprobamos que se tengan el mismo tamaño
+    if len(lista1) != len(lista2):
+        return False
+    return True
+    
 
-return True
+def compararContenido(lista1, lista2):
+    for i in range(len(lista1)):   # Recorremos letra a letra
+        if lista1[i].lower() != lista2[i].lower():    # Convertimos en minuscualas para comprobar
+            return False
+    return True
 
-print("Comprobar A y B:", comparar(a, b))
-print("Comprobar A y C:", comparar(a, c))
-print("Comprobar B y C:", comparar(b, c))
+
+
+print("Comprobar contenido:")
+print("A y B:", compararContenido(a, b))
+print("A y C:", compararContenido(a, c))
+print("B y C:", compararContenido(b, c))
+print("A y D:", compararContenido(a, d))
+
+
+print("\nComprobar tamaño:")
+print("A y B:", compararTamano(a, b))
+print("A y C:", compararTamano(a, c))
+print("B y C:", compararTamano(b, c))
+print("A y D:", compararTamano(a, d))
 
 
 
