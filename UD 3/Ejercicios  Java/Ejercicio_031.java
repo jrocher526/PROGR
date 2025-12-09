abfd80b8ffbae1f2f8087fd0ec6adfa948aea626
Jhonal Roca <jrocher526@g.educaand.es>
2025-12-09 13:46:46 +0100

    Add files via upload

---
+++ UD 3/Ejercicios  Java/Ejercicio_031.java
@@ -0,0 +1,15 @@
+package ejerciciosDeClase;
+
+public class Ejercicio_031 {
+	public static void variosSaludos(int n) {
+	    for (int i = 0; i < n; i++) {
+	        System.out.println("Hola");
+	    }
+	}
+
+	
+	public static void main(String[] args) {
+		variosSaludos(3);
+	}
+
+}
