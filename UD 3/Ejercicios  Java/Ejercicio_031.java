cc7680339d31092974ba55a7c883ac2585c6fa0e
Jhonal Roca <jrocher526@g.educaand.es>
2025-12-09 13:48:38 +0100

    Double input in variosSaludos method
    Modified the variosSaludos method to double the input parameter before printing greetings.

--- UD 3/Ejercicios  Java/Ejercicio_031.java
+++ UD 3/Ejercicios  Java/Ejercicio_031.java
@@ -2,6 +2,7 @@ package ejerciciosDeClase;
 
 public class Ejercicio_031 {
 	public static void variosSaludos(int n) {
+		n = n * 2;
 	    for (int i = 0; i < n; i++) {
 	        System.out.println("Hola");
 	    }
@@ -13,3 +14,4 @@ public class Ejercicio_031 {
 	}
 
 }
+
