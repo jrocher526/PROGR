2686fdf10baf7650429ca17437ce2b37eb5fa21a
Jhonal Roca <jrocher526@g.educaand.es>
2026-01-27 14:52:47 +0100

    Add files via upload

---
+++ UD 5/Ejercicios/ejercicio7/Concesionario.java
@@ -0,0 +1,9 @@
+package ejercicio7;
+
+public class Concesionario {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ UD 5/Ejercicios/ejercicio7/Vehiculo.java
@@ -0,0 +1,13 @@
+package ejercicio7;
+
+public class Vehiculo {
+
+	// Atributos 
+	private String marca;
+	private String modelo;
+	private int anio;
+	
+	Vehiculo(String marca, String modelo, int anio) {
+		
+	}
+}
